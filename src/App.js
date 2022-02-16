769aef49129cf02ec353519b041ba016b086160b
ks-mani <kumar.mani017@gmail.com>
2022-02-16 16:00:11 +0530

    News Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import NewsComponent from './News/NewsComponent';
 
 function App() {
   return (
     <div className='container d-flex justify-content-center'>
       <div className='app-container'>
         <h1 className='header'>News Today</h1>
-        <p>Mani</p>
+        <NewsComponent />
       </div>
     </div>
-    // <div className="App mt-4">
-    //   <p>Mani Kumar</p>      
-    // </div>
   );
 }
 
---
+++ src/News/NewsComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default function NewsComponent() {
+    return (
+        <p>Mani Kumar Srivastava</p>
+    )
+}
+
