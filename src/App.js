2cb3e9fe821444e7e8cc655b63ac811a35f2677a
ks-mani <kumar.mani017@gmail.com>
2022-02-16 14:55:17 +0530

    Some basic setup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App mt-4">
-      <p>Mani Kumar</p>      
+    <div className='container d-flex justify-content-center'>
+      <div className='app-container'>
+        <div style={{width: '100%', height: '200px', background: 'green' }}></div>
+      </div>
     </div>
+    // <div className="App mt-4">
+    //   <p>Mani Kumar</p>      
+    // </div>
   );
 }
 
