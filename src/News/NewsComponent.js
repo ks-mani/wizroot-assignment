9c48dc2b2d0e5e033784922ab9538f17ac3fe507
ks-mani <kumar.mani017@gmail.com>
2022-02-16 18:31:59 +0530

    Categories List UII

--- src/News/NewsComponent.js
+++ src/News/NewsComponent.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './NewsComponent.css';
+import plus from '../assets/icon_ionic-ios-add.svg';
+
+import { news_categories } from '../Data/constants';
 
 
 export default function NewsComponent() {
+    const [categories,setCategories] = useState(news_categories);
+
     return (
-        <p>Mani Kumar Srivastava</p>
+        <>
+            <ul className='category-list'>
+                {categories.map((item, index)=>{
+                    if(index===0) return <li className='category-list-item active-item'>{item.name}</li>
+                    return <li className='category-list-item'>{item.name}</li>
+                })}
+                <li className='category-list-item'><img src={plus} /></li>
+            </ul>
+        </>
     )
 }
 
