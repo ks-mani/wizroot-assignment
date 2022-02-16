b596817214f505a917bcb72345be056553f21bbf
ks-mani <kumar.mani017@gmail.com>
2022-02-16 14:23:55 +0530

    Installed Bootstrao

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App mt-4">
       <p>Mani Kumar</p>      
     </div>
   );
